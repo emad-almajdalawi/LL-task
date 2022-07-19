0474102405c147c41510a70ad23a8005622fe525
emad-almajdalawi <e.almajdalawi@gmail.com>
2022-07-19 08:22:50 -0400

    Feat! : add

--- ll.py
+++ ll.py
@@ -20,3 +20,27 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+
+    def add(self, data):
+        """
+        Create new node then add it to the end of the linked-list
+        Input: data (a new node will be created using this data)
+        Output: Nothing
+        """
+        try:
+            if isinstance(data, Node):
+                raise TypeError
+        except TypeError:
+            return (
+                "Please enter the data and it will be converted to a Nod eautomatically"
+            )
+        else:
+            new_node = Node(data)
+            if self.head is None:
+                self.head = new_node
+            else:
+                current = self.head
+                while not current.next1 in None:
+                    current = current.next1
+                current.next1 = new_node
+                current.next2 = new_node
